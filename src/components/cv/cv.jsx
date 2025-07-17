524297d376dd91b4c2f78f002849f25e6dc911db
Vangelis-26 <mourier.matthieu@gmail.com>
2025-07-17 14:47:48 +0200

    fix: Corriger le style du bouton de téléchargement du CV

--- src/components/cv/cv.jsx
+++ src/components/cv/cv.jsx
@@ -5,9 +5,9 @@ export default function Cv() {
         <a
             href="/Mourier Matthieu CV.pdf"
             download
-            className="group flex items-center justify-center gap-2 px-6 py-3 rounded-md font-semibold text-center bg-gradient-to-r from-(--color-border) to-purple-600 transform transition-all duration-300 ease-in-out hover:-translate-y-0.5 hover:shadow-lg hover:shadow-(--color-border)/40"
+            className="group flex items-center justify-center gap-2 px-6 py-3 rounded-md font-semibold text-center bg-(--color-border) transform transition-all duration-300 ease-in-out hover:-translate-y-0.5 hover:shadow-lg hover:shadow-(--color-border)/40"
         >
-            <FiDownload className="transition-transform duration-300 group-hover:-rotate-12" />
+            <FiDownload className="transition-transform duration-300" />
             <span>CV</span>
         </a>
     );
