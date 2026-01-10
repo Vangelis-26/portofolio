dfa58f316180ef7c63dd3bffa5fe54032dd326c5
Vangelis-26 <mourier.matthieu@gmail.com>
2026-01-10 01:51:07 +0100

    fix link projet

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -108,7 +108,7 @@ export default function Header() {
                         style={{ animationDelay: '400ms' }}
                      >
                         <Link
-                           href="/projects"
+                           href="/projets"
                            className="w-full sm:w-auto px-6 py-2.5 rounded-md font-semibold bg-(--color-border) text-white md:transform md:transition-transform md:hover:scale-105"
                         >
                            Mes projets
